f390f87958e9650209ff635064983f4910cafb1d
Peter Hamer <peter.hamer@student.manchester.ac.uk>
2021-11-15 20:55:06 +0000

    Add comments + Remove Debug Statements

--- py_twit.py
+++ py_twit.py
@@ -1,31 +1,44 @@
 from os import environ
-import glob, random, tweepy, time
+import glob
+import random
+import tweepy
+import time
 
+
+# Gets two random .png files from the folder given as a parameter.
 def RandomSWChars(folder):
         file_path_type = [folder + "*.png"]
         images = glob.glob(random.choice(file_path_type))
         return [random.choice(images), random.choice(images)]
 
+# Establishes credentials for twitter api access
+# Defined through heroku
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 
-TWEET_INTERVAL = 60 * 30 # tweet every 30 mins
+# Interval between tweets, (60 * 30) = 30 minutes
+TWEET_INTERVAL = 60 * 30
 
+# Authorise twitter API connection
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# Tweet a battle, scheduled with TWEET_INTERVAL
 while True:
-	sw_chars = RandomSWChars("sw_char/")
-	media_ids = [api.media_upload(i).media_id_string for i in sw_chars]
+    # Add character images to the tweet
+    sw_chars = RandomSWChars("sw_char/")
+    media_ids = [api.media_upload(i).media_id_string for i in sw_chars]
 
-	for i in range(len(sw_chars)):
-	    sw_chars[i] = sw_chars[i].replace('sw_char/', '').replace('.png', '')
+    # Extract character names from .png file names.
+    for i in range(len(sw_chars)):
+        sw_chars[i] = sw_chars[i].replace('sw_char/', '').replace('.png', '')
 
-	tweet_text = "STAR WARS DEATH BATTLE!\n" + sw_chars[0] + " VS " + sw_chars[1]
+    # Post tweet
+    tweet_text = "STAR WARS DEATH BATTLE!\n" + sw_chars[0] + " VS " + sw_chars[1]
+    api.update_status(status=tweet_text, media_ids=media_ids)
 
-	api.update_status(status=tweet_text, media_ids=media_ids)
-
-	time.sleep(TWEET_INTERVAL)
+    # Wait specified amount of time, then repeat tweeting process
+    time.sleep(TWEET_INTERVAL)
--- webScrape.py
+++ webScrape.py
@@ -1,68 +1,62 @@
 import os
-import selenium
-from selenium import webdriver
-import time
-#from PIL import Image
 import io
-#import requests
+import time
+import urllib
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import urllib
 
-#Install Driver
+# Install Driver
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-#Specify Search URL 
-search_url="""https://www.starwars.com/databank/"""
-
+# Navigate to Official Star Wars Databank
+search_url = """https://www.starwars.com/databank/"""
 driver.get(search_url)
 
+# Filter to characters
 driver.find_elements_by_xpath("""//*[@id="ref-1-6"]/div[1]/div[2]/ul/li[2]/span""")[0].click()
 
-i=0
+i = 0
 exit = 0
+
+# Create folder for character images
 if not os.path.exists('sw_char'):
-	os.mkdir('sw_char')
+    os.mkdir('sw_char')
+
+# Clicks 'Load More' until the option is no longer available.
 while True:
-	time.sleep(1.5)
+    time.sleep(1.5)
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, """/html/body/div[5]/div[1]/div[2]/div[2]/article/section[7]/div[1]/div[4]/div/a/span[1]""")))
 
-	try:
-		element = WebDriverWait(driver, 5).until(
-			EC.element_to_be_clickable((By.XPATH, """/html/body/div[5]/div[1]/div[2]/div[2]/article/section[7]/div[1]/div[4]/div/a/span[1]""")))
+        more = driver.find_elements_by_xpath("""/html/body/div[5]/div[1]/div[2]/div[2]/article/section[7]/div[1]/div[4]/div/a/span[1]""")
+        more[0].click()
+    except:
+        # 'Load More' not clickable
+        exit = exit + 1
+    i = i + 1
+    # 'Load More' has failed 10 times so exit the loop
+    if (exit >= 10):
+        break
 
-		more = driver.find_elements_by_xpath("""/html/body/div[5]/div[1]/div[2]/div[2]/article/section[7]/div[1]/div[4]/div/a/span[1]""")
-		more[0].click()
-	except:
-		exit = exit + 1
-
-	i=i+1
-
-	if (exit>=10):
-		break
-	print(i)
-print("images")
+# Get the character image and name for each character
+# Store image as '<character_name>.png' in the 'sw_char' folder
 for x in range(1, 836):
-	try:
-		char = driver.find_elements_by_xpath("/html/body/div[5]/div[1]/div[2]/div[2]/article/section[7]/div[1]/div[3]/div/ul[2]/div[" + str(x) + "]/div/div/div/div/div[2]/div[2]/div/h3/a/span")
-		
-		for element in char:
-			char_name = element.text
-			print(char_name)
+    try:
+        char = driver.find_elements_by_xpath("/html/body/div[5]/div[1]/div[2]/div[2]/article/section[7]/div[1]/div[3]/div/ul[2]/div[" + str(x) + "]/div/div/div/div/div[2]/div[2]/div/h3/a/span")
 
-		char_img = driver.find_elements_by_xpath("/html/body/div[5]/div[1]/div[2]/div[2]/article/section[7]/div[1]/div[3]/div/ul[2]/div[" + str(x) + "]/div/div/div/div/div[1]/div/a/img")
-		for img in char_img:
-			src = img.get_attribute('src')
+        # Get characters name
+        for element in char:
+            char_name = element.text
 
-			# download the image
-			urllib.request.urlretrieve(src, "sw_char/" + char_name + ".png")
-	except:
-		print("f")
-
-totalFiles=0
-for base, dirs, files in os.walk("sw_char"):
-    for Files in files:
-        totalFiles += 1
-print(totalFiles)
+        # Get character image
+        char_img = driver.find_elements_by_xpath("/html/body/div[5]/div[1]/div[2]/div[2]/article/section[7]/div[1]/div[3]/div/ul[2]/div[" + str(x) + "]/div/div/div/div/div[1]/div/a/img")
+        for img in char_img:
+            src = img.get_attribute('src')
+            urllib.request.urlretrieve(src, "sw_char/" + char_name + ".png")
+    except:
+        pass
